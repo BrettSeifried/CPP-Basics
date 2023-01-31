381c7a3e0f05f1e151b071f685fc87c0573c740b
Brett Seifried <seifriedbrett@gmail.com>
2023-01-31 15:13:31 -0800

    Vector exercises

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -8,12 +8,16 @@ int main( ){
 	cout << "\nThe First vowel is: " << vowels[0] << endl;
 	cout << "The last vowel is: " << vowels[4] << endl;
 	
+	//=====================
+	
 	double hi_temps [] {90.1, 89.8, 77.5, 81.6};
 	cout << "\nThe firsst high rempature is: " << hi_temps[0] << endl;
 	
 	hi_temps[0] = 100.7;
 	
 	cout<< "The first high temperature is now: " << hi_temps[0] << endl;
+	
+	// =========================
 	
 	int test_scores [] {100, 90, 80, 70, 60};
 	
