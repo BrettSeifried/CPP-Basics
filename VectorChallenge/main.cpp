f2ed21b9c438d2e82fb8659d8219f49b263718a2
Brett Seifried <seifriedbrett@gmail.com>
2023-01-31 15:39:08 -0800

    finished vector challenge

--- VectorChallenge/main.cpp
+++ VectorChallenge/main.cpp
@@ -12,7 +12,7 @@ int main( ){
 	
 	cout << "\nVector1's first int is: " << vector1.at(0) << endl;
 	cout << "Vector1's Second int is: " << vector1.at(1) << endl;
-	cout << "Size of the vector is: " << vector1.size() << endl;
+	cout << "Size of the vector1 is: " << vector1.size() << endl;
 	
 	vector2.push_back(100);
 	vector2.push_back(200);
@@ -23,11 +23,13 @@ int main( ){
 	
 	vector <vector<int>> vector_2d {};
 	
-	vector_2d.push_back(vector1(0));
-	vector_2d.push_back(vector2(0));
+	vector_2d.push_back(vector1);
+	vector_2d.push_back(vector2);
 	
-	cout << "\nVector_2d's 1st int is: " << vector_2d.at(0) << endl;
-	cout << "Vector_2d's 2nd int is: " << vector_2d.at(1) << endl;
+	cout << "\nVector_2d's row 1, col 1: " << vector_2d.at(0).at(0) << endl;
+	cout << "\nVector_2d's row 1, col 2: " << vector_2d.at(0).at(1) << endl;
+	cout << "Vector_2d's row 2, col 1: " << vector_2d.at(1).at(0) << endl;
+	cout << "Vector_2d's row 2, col 2: " << vector_2d.at(1).at(1) << endl;
 	
 	return 0;
 }
